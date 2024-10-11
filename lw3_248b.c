1ea1dbeefb6245b4244f4ab6a88c8dbbbea419ad
AbsoluteVirtue <3hitemouse@gmail.com>
2024-10-11 16:22:26 +0300

    Update lw3_248b.c

--- lw3_248b.c
+++ lw3_248b.c
@@ -1,1 +1,26 @@
+/*
 
+*/
+
+#include <stdio.h>
+
+int main( void )
+{
+    int rows = 0, cols = 0;
+    scanf("%d%d", &rows, &cols);
+
+    const int NUM_ROWS = rows;
+    const int NUM_COLS = cols;
+    int matrix[NUM_ROWS][NUM_COLS];
+
+    for (size_t i = 0; i < NUM_ROWS; i++)
+    {
+        for (size_t j = 0; j < NUM_COLS; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    
+    // алгоритм и вывод результата здесь
+
+}
